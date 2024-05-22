1882ed9f13fb6e34c285585fa9626ac69767f159
masaaki <haradam_mu@yahoo.co.jp>
2024-05-22 11:53:33 +0900

    account-lock

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,9 +6,15 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
+
+    public function __construct(User $user){
+      $this->user = $user;
+    }
+
     /* @return View  */
     public function showLogin(){
         return view('login.login_form');
@@ -17,23 +23,43 @@ class AuthController extends Controller
     /* @param App\Http\Requests\LoginFormRequest $request */
     public function login(LoginFormRequest $request){
        $credentials = $request->only(['email','password']);
-       if (Auth::attempt($credentials)) {
-          $request->session()->regenerate();
-          return redirect()->route('home')->with('success','ログイン成功');
+       $user = $this->user->getUserByEmail($credentials['email']);
+       if(!is_null($user)){
+         if ($this->user->isAccountLocked($user)){
+              return back()->withErrors([
+             'danger' => 'アカウントがロックされています.',
+           ]);
+         }
+
+         if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            $userModel = new User();
+            $this->user->resetErrorCount($user);
+            return redirect()->route('home')->with('success','ログイン成功');
+         }
+
+         $user->error_count = $this->user->addErrorCount($user->error_count);
+         if( $this->user->lockAccount($user) ){
+             $this->user->locked_flg = 1;
+             return back()->withErrors([
+               'danger' => 'アカウントをロックしました. 解除したい場合は運営者に連絡してください.',
+             ]);
+         }
+         $user->save();
        }
+
        return back()->withErrors([
          'danger' => 'メールアドレスかパスワードが間違っています.',
        ]);
     }
 
-   /* @param  Illuminate\Http\Request  $request
-      @return Illuminate\Http\Response          */
-   public function logout(Request $request)   {
-     Auth::logout();
-     $request->session()->invalidate();
-     $request->session()->regenerateToken();
-     return redirect()->route('showLogin')->with('danger','ログアウト');
-   } 
-
+    /* @param  Illuminate\Http\Request  $request
+       @return Illuminate\Http\Response          */
+    public function logout(Request $request)   {
+      Auth::logout();
+      $request->session()->invalidate();
+      $request->session()->regenerateToken();
+      return redirect()->route('showLogin')->with('danger','ログアウト');
+    } 
 
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,66 @@ class User extends Authenticatable
 //      'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+
+
+
+
+    /** emailがマッチしたユーザを返す
+      * @param string $email
+      * @return object(user)
+      */
+    public function getUserByEmail($email)
+    {
+      return User::where('email','=',$email)->first();
+    }
+
+    /** アカウントがロックされているか？
+      * @param object $user
+      * @return bool
+      */
+    public function isAccountLocked($user)
+    {
+      if ($user->locked_flg === 1) {
+        return true;
+      }
+      return false;
+    }
+
+    /**  エラーカウントをリセットする
+      *  @param object $user
+      **/
+    public function resetErrorCount($user)
+    {
+      if ($user->error_count > 0){
+        $user->error_count = 0;
+        $user->save();
+      }
+    }
+
+    /** エラーカウントを1増やす 
+      * @param
+      * @return 
+      */
+    public function addErrorCount($error_count)
+    {
+       return $error_count + 1;
+    }
+
+    /** アカウントをロックする
+      * @param object $user
+      * @return bool 
+      */
+    public function lockAccount($user)
+    {
+       if($user->error_count>5){
+          $user->locked_flg = 1;
+          return $user->save();
+       }
+       return false;
+    }
+
+
+
 }
